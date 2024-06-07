c27b80e5d61304e96d8a738195d99f21baaa9e2a
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-06-07 23:28:34 +0100

    Update IPL 2023 and 2024 Data Analysis.py
    Finding the top 5 run getters and later sorting the data according to the number of innings played in descending order

--- IPL 2023 and 2024 Data Analysis.py
+++ IPL 2023 and 2024 Data Analysis.py
@@ -5,3 +5,6 @@ import seaborn as sns
 BATSMEN PERFORMANCE IN IPL 2023
 org_2023 = pd.read_csv('Orange Cap 2023.csv')
 org_2023
+org_2023.head()
+innings_2023 = org_2023.sort_values(by= 'Innings', ascending = False).head(50)
+innings_2023
