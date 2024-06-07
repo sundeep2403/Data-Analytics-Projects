89d1835afd5f8279f810de950ae88d68bad8751c
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-06-07 23:32:02 +0100

    Update IPL 2023 and 2024 Data Analysis.py
    To find out the teams of the top 16 six hitters of IPL 2023

--- IPL 2023 and 2024 Data Analysis.py
+++ IPL 2023 and 2024 Data Analysis.py
@@ -11,3 +11,5 @@ innings_2023
 sixes_2023 = org_2023.sort_values(by = '6s', ascending = False).head(16)
 sixes_2023_filtered = sixes_2023[['Player Name','Team Name','6s']]
 sixes_2023_filtered
+sixes_2023.groupby('Team Name').size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
+plt.gca().spines[['top', 'right',]].set_visible(False)
