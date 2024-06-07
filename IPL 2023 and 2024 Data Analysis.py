4e050a4bc9f0d7161ee43212f0d55ed9c4ce73af
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-06-07 23:27:17 +0100

    Update IPL 2023 and 2024 Data Analysis.py
    Opening the file consisting the data for Batsmen performance in IPL 2023

--- IPL 2023 and 2024 Data Analysis.py
+++ IPL 2023 and 2024 Data Analysis.py
@@ -2,3 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+BATSMEN PERFORMANCE IN IPL 2023
+org_2023 = pd.read_csv('Orange Cap 2023.csv')
+org_2023
